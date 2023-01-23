152be7c1a9c134408b2da0c5c48805e0d291c621
Aliceson Phillips <alicesonphillips@gmail.com>
2023-01-22 20:12:52 -0500

    Update NASAphoto.js

--- src/Components/NASAphoto.js
+++ src/Components/NASAphoto.js
@@ -1,20 +1,56 @@
 import React from 'react';
+import styled from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
-// const dummyData = {
-//     date: "2023-01-21",
-//     explanation: "Comet C/2022E3 (ZTF) is no longer too dim to require a telescope for viewing. By January 19, it could just be seen with the naked eye in this rural sky with little light pollution from a location about 20 kilometers from Salamanca, Spain. Still, telescopic images are needed to show any hint of the comet's pretty green coma, stubby whitish dust tail, and long ion tail. Its faint ion tail has been buffeted by recent solar activity. This visitor from the distant Oort cloud rounded the Sun on January 12. and is now sweeping through stars near the northern boundary of the constellation Bootes. Outward bound but still growing brighter, Comet ZTF makes its closest approach on February 2, coming to within about 2.4 light-minutes of our fair planet.",
-//     hdurl: "https://apod.nasa.gov/apod/image/2301/ZTF_salamanca.jpg",
-//     title: "Naked-eye Comet ZTF"    
-// }
+const GlobalStyle = createGlobalStyle`
+   body{
+    font-family: 'Anton', sans-serif;
+    font-family: 'Unbounded', cursive;
+   }
+`;
+const NasaStyle = styled.div`
+    padding: 3px 5px;
+    margin: 5px 10px;
+    background-color: "lightyellow";
+    font-family: 'Unbounded', cursive;
+
+
+`;
+
+const NasaHeading = styled.h3`
+    font-size: 35px;
+    font-weight: 600;
+    background-image: linear-gradient(to left, #553c9a, #b393d3);
+    color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+    font-family: 'Anton', sans-serif;
+
+`;
+const Nasap = styled.p`
+    font-size: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    border: 3px grey dotted;
+    padding: 5px;
+    font-family: 'Unbounded', cursive;
+    /* From https://css.glass */
+    background: rgba(72, 137, 206, 0.55);
+    /* border-radius: 16px; */
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(8.9px);
+    -webkit-backdrop-filter: blur(8.9px);
+    /* border: 3px solid rgba(72, 137, 206, 1); */
+`;
 
 const NasaPhoto = (props) => {
     return (
-        <div className="nasa-photo-wrapper">
-            <h3>{props.photo.title}</h3>
-            <p>{props.photo.date}</p>
+        <NasaStyle className="nasa-photo-wrapper">
+            <NasaHeading>{props.photo.title}</NasaHeading>
             <img src={props.photo.hdurl} />
-            <p className='explanation'>{props.photo.explanation}</p>
-        </div>
+            <Nasap className='explanation'>{props.photo.explanation}</Nasap>
+            <Nasap>{props.photo.date}</Nasap>
+        </NasaStyle>
     )
 }
 
